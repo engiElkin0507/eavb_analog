d89a5ae3d4ef0003ccebc93a29ec3c01f1566588
Elcyn Andrew V. Booc <138495166+engiElkin0507@users.noreply.github.com>
2026-08-14 20:16:41 +0800

    Update __init__.py

--- micropython/eavb_analog/__init__.py
+++ micropython/eavb_analog/__init__.py
@@ -1,1 +1,9 @@
-# eavb_analog package
+# micropython/eavb_analog/__init__.py
+
+from .core import ADCScale
+from .scaling import VoltageDivider, HardwareSolver, ScaledSensor
+from .filters import MovingAverage, ExponentialFilter
+from .telemetry import TelemetryStreamer
+
+__version__ = "1.0.0"
+__author__ = "Elcyn Andrew V. Booc"
